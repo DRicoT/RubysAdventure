1d568b053090c3e4e514bae82e0facb62b9b58ad
DRicoT <wasnaisxp@gmail.com>
2022-03-07 12:54:37 +0100

    adding_enemies

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    Rigidbody2D rigidbody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + Time.deltaTime * speed;
+        
+        rigidbody2D.MovePosition(position);
+    }
+}
